1d037f9db817fe214336c5171ff728212644b302
unknown <mcstevensonchukwuneye@gmail.com>
2025-03-26 12:44:25 +0100

    Operators

--- UserInput.java
+++ UserInput.java
@@ -7,16 +7,29 @@ public class UserInput{
 		System.out.print("Enter your name: ");
 		String name = input.nextLine();
 		
+		System.out.print("Enter gender (M/F): ");
+		char gender = input.next().charAt(0);
+		
+		input.nextLine();
+		
 		System.out.print("Enter your address: ");
 		String address = input.nextLine();
 		
 		System.out.print("Enter your age: ");
 		byte age = input.nextByte();
 		
-		System.out.println();
+		System.out.print("Enter your balance: ");
+		double balance = input.nextDouble();
 		
+		System.out.print("Do you love Java? (True/false): ");
+		boolean isJava = input.nextBoolean();
+		
+		System.out.println();
 		System.out.printf("Hello %s, You are welcome to First Bank%n",name);
+		System.out.printf("Your gender is %c%n",gender);
 		System.out.printf("You are living in %s%n",address);
-		System.out.printf("You  are %d years old",age);
+		System.out.printf("You  are %d years old%n",age);
+		System.out.printf("Your account bablance is %c%.2f%n",'$',balance);
+		System.out.printf("%s, do you love Java? %b%n",isJava,name);
 	}
 }
